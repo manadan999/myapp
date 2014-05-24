9075e0f3ed89ea56b6b99c1d3ead08220487aa68
Joseph Joy Manadan <josephmanadan@gmail.com>
2014-05-24 13:13:52 +0100

    ionic template commit

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function($scope) {
 })
 
+
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
